5bac07f82ba6920d7f1a918914575e6b1e0b4710
shunsei <k.shunsei1227@gmail.com>
2025-08-31 13:55:16 +0900

    refactor main

--- src/main.py
+++ src/main.py
@@ -1,51 +1,69 @@
 from datetime import date, timedelta
 
+from logger import configure_logger, get_logger
 from models.plan import ReservationPlan
 from notifier import LINENotifier
 from webdriver import Kaitabi20sIzumoWebDriver
 
 
 def main():
-    webdriver = Kaitabi20sIzumoWebDriver(headless=True)
+    configure_logger()
     notifier = LINENotifier()
 
-    is_candidate_plan_found = False  # ダイアログが出ないプランが見つかったかどうか
+    try:
+        webdriver = Kaitabi20sIzumoWebDriver(headless=True)
 
-    for i in range(1, 31):  # 今日から30日後まで調べる
-        check_in_date = date.today() + timedelta(days=i)
-        plan = ReservationPlan(
-            check_in=check_in_date,
-            stay=1,
-            adults=2,
-            children_7_11=0,
-            children_4_6=0,
-            children_0_6=0,
-        )
+        is_candidate_plan_found = False  # ダイアログが出ないプランが見つかったかどうか
 
-        webdriver.visit(plan)
+        for i in range(1, 31):  # 今日から30日後まで調べる
+            check_in_date = date.today() + timedelta(days=i)
+            plan = ReservationPlan(
+                check_in=check_in_date,
+                stay=1,
+                adults=2,
+                children_7_11=0,
+                children_4_6=0,
+                children_0_6=0,
+            )
 
-        if not webdriver.is_alert_dialog_displayed():
-            is_candidate_plan_found = True
-            break
+            webdriver.visit(plan)
 
-    if not is_candidate_plan_found:
+            if not webdriver.is_alert_dialog_displayed():
+                is_candidate_plan_found = True
+                break
+
+        if not is_candidate_plan_found:
+            notifier.notify("予約可能なプランが見つかりませんでした")
+            return
+
+        available_check_in_date = webdriver.find_available_check_in_date()
+
+        if available_check_in_date is not None:
+            notifier.notify(
+                f"予約可能なプランが見つかりました: {available_check_in_date.strftime('%Y/%m/%d')}"
+            )
+            return
+
+        webdriver.click_next_month_button()
+        available_check_in_date = webdriver.find_available_check_in_date()
+
+        if available_check_in_date is not None:
+            notifier.notify(
+                f"予約可能なプランが見つかりました: {available_check_in_date.strftime('%Y/%m/%d')}"
+            )
+            return
+
         notifier.notify("予約可能なプランが見つかりませんでした")
-        return
 
-    available_plan = webdriver.find_available_plan()
+    except Exception as e:
+        notifier.notify("予約チェック中にエラーが発生しました")
 
-    if available_plan is not None:
-        notifier.notify(f"予約可能なプランが見つかりました: {available_plan}")
-        return
+        logger = get_logger()
+        logger.error("予約チェック中にエラーが発生しました")
+        logger.error(e)
 
-    webdriver.click_next_month_button()
-    available_plan = webdriver.find_available_plan()
-
-    if available_plan is not None:
-        notifier.notify(f"予約可能なプランが見つかりました: {available_plan}")
-        return
-
-    notifier.notify("予約可能なプランが見つかりませんでした")
+    finally:
+        webdriver.close()
 
 
 if __name__ == "__main__":
